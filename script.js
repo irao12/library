3cc61e6925df9f59fb7c3cc8a1bae41c882d88dd
irao12 <ivanrao@outlook.com>
2022-07-11 18:39:36 -0400

    added display for adding books

--- script.js
+++ script.js
@@ -13,20 +13,25 @@ function addBookToLibrary(book){
 
 function displayBook(book) {
     const bookDiv = document.createElement("div");
-    bookDiv.innerHTML = '<h2>' + book.title + '</h2>' +
-                        '<p>by ' + book.author + '</p>';
+    bookDiv.innerHTML = '<div> <h2>' + book.title + '</h2>' +
+                        '<p>by ' + book.author + '</p> </div>' + 
+                        '<p>page count: ' + book.pages + '</p>' +
+                        '<p>status: ' + (book.read ? 'finished' : 'not finished') + '</p>' +
+                        '</div>';
+                        
+
     bookDiv.classList.add("book-div");
 
-    const mainContent = document.querySelector('.main');
-    mainContent.appendChild(bookDiv);
+    const add = document.querySelector('.add-div');
+    add.parentNode.insertBefore(bookDiv, add);
 }
 
 function displayLibrary () {
     myLibrary.forEach(displayBook);
 }
 
-const book1 = new Book ("The Great Gatsby", "F. Scott Fitzgerald", false);
-const book2 = new Book ("Because of Winn-Dixie", "Kate DiCamillo", false);
+const book1 = new Book ("The Great Gatsby", "F. Scott Fitzgerald", 208, false);
+const book2 = new Book ("Because of Winn-Dixie", "Kate DiCamillo", 182, false);
 
 addBookToLibrary(book1);
 addBookToLibrary(book2);
