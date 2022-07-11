2315c28d006364ee5e0da8f031930204db2862a3
irao12 <ivanrao@outlook.com>
2022-07-11 17:59:13 -0400

    added the ability to display books from the library

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let myLibrary = [];
 
-function Book(name, author, read) {
-    this.name = name;
+function Book(title, author, pages, read) {
+    this.title = title;
     this.author = author;
+    this.pages = pages;
     this.read = read;
 }
 
@@ -10,8 +11,24 @@ function addBookToLibrary(book){
     myLibrary.push(book);
 }
 
+function displayBook(book) {
+    const bookDiv = document.createElement("div");
+    bookDiv.innerHTML = '<h2>' + book.title + '</h2>' +
+                        '<p>by ' + book.author + '</p>';
+    bookDiv.classList.add("book-div");
+
+    const mainContent = document.querySelector('.main');
+    mainContent.appendChild(bookDiv);
+}
+
+function displayLibrary () {
+    myLibrary.forEach(displayBook);
+}
+
 const book1 = new Book ("The Great Gatsby", "F. Scott Fitzgerald", false);
+const book2 = new Book ("Because of Winn-Dixie", "Kate DiCamillo", false);
 
 addBookToLibrary(book1);
+addBookToLibrary(book2);
 
-console.log(myLibrary)
+displayLibrary();
